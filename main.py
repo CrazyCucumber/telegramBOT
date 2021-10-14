570d749ab39ab5ac31b5b572f19afdb4fc51efc6
CrazyCucumber <dimavolkov_work@mail.ru>
2021-10-14 13:59:06 +0300

    Add git actions

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from functions_student import registration, DatabaseEntry
 from functions_teacher import data_check
 
 from config import TOKEN
-bot = telebot.TeleBot(TOKEN)
 
+bot = telebot.TeleBot(TOKEN)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard1.row('Студент', 'Преподаватель')
@@ -13,7 +13,8 @@ keyboard1.row('Студент', 'Преподаватель')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nВыбери кто ты: студент или преподаватель', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\nВыбери кто ты: студент или '
+                                      f'преподаватель', reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
@@ -26,14 +27,17 @@ def send_text(message):
     elif message.text == 'Преподаватель':
         msg = bot.send_message(message.chat.id, f'{message.from_user.first_name}, 1 - Посмотреть Базу данных \n')
         bot.register_next_step_handler(msg, teacher_next_step)
+
+
 def student_register(message):
     registration(message)
     DatabaseEntry(message)
+
+
 def teacher_next_step(message):
     if message.text == '1':
         data_check(message)
 
 
-
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
